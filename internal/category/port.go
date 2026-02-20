7c5ba3ed71d23ff1f9d7d3b59fdd9398d3ce27b4
noyandey88 <coder.noyan@gmail.com>
2026-02-21 00:16:38 +0600

    feat: category service has been created

--- internal/category/port.go
+++ internal/category/port.go
@@ -1,1 +1,18 @@
 package category
+
+import (
+	"github.com/noyandey88/blog-api/domain"
+	categoryHandler "github.com/noyandey88/blog-api/rest/handlers/category"
+)
+
+type Service interface {
+	categoryHandler.Service
+}
+
+type CategoryRepo interface {
+	Create(category domain.Category) (*domain.Category, error)
+	FindAll() (*[]domain.Category, error)
+	FindById(id int) (*domain.Category, error)
+	Update(category domain.Category) (*domain.Category, error)
+	Delete(id int) error
+}
--- internal/category/service.go
+++ internal/category/service.go
@@ -1,1 +1,80 @@
 package category
+
+import "github.com/noyandey88/blog-api/domain"
+
+type service struct {
+	categoryRepo CategoryRepo
+}
+
+func NewService(categoryRepo CategoryRepo) Service {
+	return &service{
+		categoryRepo: categoryRepo,
+	}
+}
+
+func (svc *service) Create(category domain.Category) (*domain.Category, error) {
+	ctgry, err := svc.categoryRepo.Create(category)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if ctgry == nil {
+		return nil, nil
+	}
+
+	return ctgry, nil
+
+}
+
+func (svc *service) FindAll() (*[]domain.Category, error) {
+	categories, err := svc.categoryRepo.FindAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if categories == nil {
+		return nil, nil
+	}
+
+	return categories, nil
+}
+
+func (svc *service) FindById(id int) (*domain.Category, error) {
+	category, err := svc.categoryRepo.FindById(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if category == nil {
+		return nil, nil
+	}
+
+	return category, nil
+}
+
+func (svc *service) Update(category domain.Category) (*domain.Category, error) {
+	ctgry, err := svc.categoryRepo.Update(category)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if ctgry == nil {
+		return nil, nil
+	}
+
+	return ctgry, nil
+}
+
+func (svc *service) Delete(id int) error {
+	err := svc.categoryRepo.Delete(id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
